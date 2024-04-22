bddffdf8ef2d1ed6be5b4867c39b4550ebfa70ac
Anar Ismayilov <92791089+developer2507@users.noreply.github.com>
2024-04-22 13:59:21 +0200

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h!583fxiwx*s6y@9&$&xt--o^^ak1$uv78^g0w*)3ek!1(k=l)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
